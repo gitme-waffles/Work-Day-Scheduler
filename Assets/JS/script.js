1ac44134011cb7495ea876a9af767e4146d9665d
Doss Anania <dossanania@gmail.com>
2021-09-23 21:17:34 +1000

    Add notes and tidy code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,6 +2,7 @@
 var weekDay = moment().format("do MMMM, YYYY");
 $("#currentDay").text(weekDay);
 
+// textarea of timeblock
 var hour9 = $("#hour-9");
 var hour10 = $("#hour-10");
 var hour11 = $("#hour-11");
@@ -11,14 +12,14 @@ var hour14 = $("#hour-14");
 var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
+var timeArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+
 var saveBtn = $(".saveBtn")
 
-// time
+// time check for colour changes
 var currentTime = moment()
 const offSet = 9;
 currentTime.hour()
-
-var timeArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
 
 for (let i = 0; i < timeArray.length; i++) {
     let checkedTime = i + offSet;
@@ -31,22 +32,20 @@ for (let i = 0; i < timeArray.length; i++) {
     }
 }
 
-// get key from local storage render to text area (if there is text or not?)
 function renderText(params) {
     for (let i = 0; i < timeArray.length; i++) {
         var keyName = timeArray[i].attr('id');
         var renderReady = localStorage.getItem(keyName);
-        if (null != renderReady) {
+        if (renderReady != null) {
            timeArray[i].val(renderReady);
         }      
-    }    
+    }
 }
 
 function saveText(event) {
     var target = $(event.currentTarget.previousElementSibling)
     var keyName = target.attr('id');
     localStorage.setItem(keyName, target.val());
-    console.log(keyName);
 }
 
 $(".saveBtn").on("click", function(event) {
